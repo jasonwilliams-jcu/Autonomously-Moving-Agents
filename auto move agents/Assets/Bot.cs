f5f1375fc9049e2c82acbada370ff88357c1e916
jasonwilliams-jcu <jason.williams1@my.jcu.edu.au>
2023-05-25 15:05:19 +1000

    Update Bot.cs for Evade

--- auto move agents/Assets/Bot.cs
+++ auto move agents/Assets/Bot.cs
@@ -7,11 +7,13 @@ public class Bot : MonoBehaviour
 {
     UnityEngine.AI.NavMeshAgent agent;
     public GameObject target;
+    Drive ds;
 
     // Start is called before the first frame update
     void Start()
     {
-        agent = this.GetComponent<NavMeshAgent>();    
+        agent = this.GetComponent<NavMeshAgent>();
+        ds = target.GetComponent<Drive>();
     }
 
     void Seek(Vector3 location)
@@ -32,19 +34,28 @@ public class Bot : MonoBehaviour
         float relativeHeading = Vector3.Angle(this.transform.forward, this.transform.TransformVector(target.transform.forward));
         float toTarget = Vector3.Angle(this.transform.forward, this.transform.TransformVector(targetDir));
 
-        if ((toTarget > 90 && relativeHeading < 20) || target.GetComponent<Drive>().currentSpeed < 0.01f)
+        if ((toTarget > 90 && relativeHeading < 20) || ds.currentSpeed < 0.01f)
         {
             Seek(target.transform.position);
             return;
         }
 
-        float lookAhead = targetDir.magnitude / (agent.speed + target.GetComponent<Drive>().currentSpeed);
+        float lookAhead = targetDir.magnitude / (agent.speed + ds.currentSpeed);
         Seek(target.transform.position + target.transform.forward * lookAhead);
+    }
+
+    void Evade()
+    {
+        Vector3 targetDir = target.transform.position - this.transform.position;
+
+        float lookAhead = targetDir.magnitude / (agent.speed + ds.currentSpeed);
+        Flee(target.transform.position + target.transform.forward * lookAhead);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Pursue();
+        Evade();
     }
 }
